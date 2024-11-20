f20fd5705200ec1ef4cec2d83398788be8030b55
rryuuni <fkdbs1122@naver.com>
2024-11-20 09:11:53 +0900

    two

--- p202114060/Main.java
+++ p202114060/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("202114060 양라윤");
+      System.out.println(Hello.sum(10));
+      System.out.println(Hello.sum(100));  
     }
   }
   
